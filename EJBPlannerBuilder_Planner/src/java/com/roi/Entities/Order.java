f521db2f078d1e23073be541d77104887584df29
rstratta <rstratta@geocom.com.uy>
2017-10-21 23:26:22 -0300

    first commit

---
+++ EJBPlannerBuilder_Planner/src/java/com/roi/Entities/Order.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.roi.Entities;
+import java.util.Date;
+/**
+ *
+ * @author strat
+ */
+public class Order {
+    public int orderId;
+    public int clientId;
+    public Date dateFrom;
+    public int voulme;
+    public int servicePointId;
+    public int billingCloseday;
+}
